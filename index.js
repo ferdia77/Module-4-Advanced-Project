8f4d587f19cf68168b41442abdbfce7bdeea17e1
ferdia77 <kingqueensuited22@gmail.com>
2025-01-07 11:11:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// template_07f5fro
+// service_hwrqlyh
+// X-z20cWkCBgFiAgPj
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('modal__overlay--loading');
+    const success = document.querySelector('modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+        .sendForm(
+            'service_hwrqlyh',
+            'template_07f5fro', 
+            event.target, 
+            'X-z20cWkCBgFiAgPj'  
+    ).then(() => { 
+        
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overly--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is currently unavailable. Please contact me direclty at kingqueensuited22@gmail.com"
+        );
+    }) 
+}

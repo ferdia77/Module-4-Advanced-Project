6f2955cde479beec7da13355e31f22fc5232179e
ferdia77 <kingqueensuited22@gmail.com>
2025-01-09 09:08:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,40 @@
 // service_hwrqlyh
 // X-z20cWkCBgFiAgPj
 
+let isModalOpen = false;
+let contrastToggle = false
+const scaleFactorr = 1/20
+
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1 ;
+        shapes[i].computedStyleMap.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if(contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('modal__overlay--loading');
-    const success = document.querySelector('modal__overlay--success');
-    loading.classList.add("modal__overlay--visible")
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
 
     emailjs
         .sendForm(
@@ -17,7 +46,7 @@ function contact(event) {
     ).then(() => { 
         
         loading.classList.remove("modal__overlay--visible");
-        success.classList.add("modal__overlay--visible");
+        success.classList += "modal__overly--visible";
     }).catch(() => {
         loading.classList.remove("modal__overlay--visible");
         alert(
@@ -26,6 +55,12 @@ function contact(event) {
     }) 
 }
 
+
 function toggleModal() {
-    document.body.classList += " modal--open"
+    if(isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.removal("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList.add("modal--open");
 }
